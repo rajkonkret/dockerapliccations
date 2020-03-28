808d4ebd05e4cea254dc9ac61198b79b8b8f065e
rajkonkret <rajkonkret660@gmail.com>
2020-03-28 13:48:34 +0100

    add CountryLanguage

--- src/main/java/com/rajkonkret/countriesdocker/controller/CountryController.java
+++ src/main/java/com/rajkonkret/countriesdocker/controller/CountryController.java
@@ -1,4 +1,15 @@
 package com.rajkonkret.countriesdocker.controller;
 
+import com.rajkonkret.countriesdocker.model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CountryController {
+    @GetMapping("/{code}")
+    public String getCountryInfo(@PathVariable String code){
+        Country country = new Country();
+
+    }
 }
---
+++ src/main/java/com/rajkonkret/countriesdocker/model/Country.java
@@ -0,0 +1,66 @@
+package com.rajkonkret.countriesdocker.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Country {
+    @Id
+    @Column(name = "code")
+    private String code;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "continent")
+    private String continent;
+    @Column(name = "population")
+    private Integer population;
+    @Column(name = "life_expectancy")
+    private Integer lifeExpectancy;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public Integer getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(Integer lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+
+
+}
